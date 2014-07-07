0b606cf64edcbc9a9f367847a79083b5d0aa2beb
Dave Leverton <dave@multipie.co.uk>
2014-07-07 15:59:37 +0100

    Updated the Layout file, and added an example of getting a textview from layout, and populating it

--- src/com/technotts/technottinghamtwitter/TweetDisplayFragment.java
+++ src/com/technotts/technottinghamtwitter/TweetDisplayFragment.java
@@ -5,19 +5,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class TweetDisplayFragment extends Fragment {
 
+	private TextView mJsonTextView; 
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_main, container,
+		View rootView = inflater.inflate(R.layout.fragment_tweet_display, container,
 				false);
+		
+		mJsonTextView = (TextView) rootView.findViewById(R.id.tweet_json_view);
+		
 		return rootView;
 	}
 	
 	public void tweetsUpdated(String tweetsJSON) {
 		// You have JSONArray of tweets here
 		// See ReadMe in github for hints.
+		mJsonTextView.setText(tweetsJSON);
 	}
 }
